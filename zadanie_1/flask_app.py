ab4f15bd77de7dfe68232f12cdc934914022b658
Patryk Janowski <pjanowski97@gmail.com>
2020-10-20 14:46:07 +0200

    Add: rev_without_text function to handle endpoint with no message to reverse

--- zadanie_1/flask_app.py
+++ zadanie_1/flask_app.py
@@ -18,5 +18,10 @@ def rev(text: str):
     return handle_rev(text)
 
 
+@flask_app.route('/rev')
+def rev_without_text():
+    return f"{ERROR_MESSAGE}, correct endpoint /rev/*your message*"
+
+
 if __name__ == '__main__':
     flask_app.run(host='127.0.0.1', port=80)
