16e2f4d6bc6dfd90d5bbd238913303c823a0a74e
Patryk Janowski <52761148+raionsakana@users.noreply.github.com>
2020-11-17 15:44:37 +0100

    Update: getting dates with events

--- zadanie_3/flask_app.py
+++ zadanie_3/flask_app.py
@@ -1,19 +1,35 @@
-from typing import Callable, Dict
-from flask import Flask, abort
+from typing import Dict
+
+from requests import get
+from bs4 import BeautifulSoup
+from flask import Flask
 
 
-class Calendar:
+class CalendarHandler:
     _VALIDATION_ERROR: str = "ERROR, given year or month is not correct data, please, check out and try again."
     _CALENDAR_URL: str = "http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?%s&lang=1"
+    _PARSER_TYPE: str = "html.parser"
 
     _JANUARY: int = 1
     _DECEMBER: int = 12
 
     def get_calendar(self, year: str, month: str):
         if self._validate(year, month):
-            return self._get_calendar_url(year, month)
+            events = self._get_events(self._get_calendar_url(year, month))
+            return ""
 
         return self._VALIDATION_ERROR
+
+    def _get_events(self, url: str) -> Dict[int, str]:
+        events = {}
+
+        soup_page = BeautifulSoup(get(url).content, self._PARSER_TYPE)
+        soup_page_list = soup_page.find_all('td', class_='active')
+
+        for i in soup_page_list:
+            print(i)
+
+        return events
 
     def _get_calendar_url(self, year: str, month: str) -> str:
         return self._CALENDAR_URL % f"rok={year}&miesiac={month}"
@@ -31,7 +47,7 @@ class Calendar:
 
 INFO_MESSAGE: str = "Info: zadanie_3"
 flask_app = Flask(__name__)
-calendar = Calendar()
+calendar = CalendarHandler()
 
 
 @flask_app.route('/')
